f83309ee26bf9b78edf1c7f8495c154f76bd9de9
G4BVEI <gabdaveiga@gmail.com>
2025-07-11 15:37:28 -0300

    coloquei placeholder no dashboard pra n dar erro

---
+++ crop-front/src/app/logged/dashboard/page.jsx
@@ -0,0 +1,12 @@
+import ImagemFundo from "@/components/ImagemFundo";
+import RegisterForm from "@/components/RegisterForm";
+
+const Register = () => {
+  return (
+    <div className="relative w-screen h-screen overflow-hidden">
+      <ImagemFundo />
+      <RegisterForm />
+    </div>
+  );
+};
+export default Register;
