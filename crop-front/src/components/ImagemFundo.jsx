8e134b80b92214de9bfc09f1ad85b8bb3f06ea06
G4BVEI <gabdaveiga@gmail.com>
2025-07-16 14:38:13 -0300

    sem a dashboard, vou ter que reformular inteira

--- crop-front/src/components/ImagemFundo.jsx
+++ crop-front/src/components/ImagemFundo.jsx
@@ -1,6 +1,6 @@
 export default function ImagemFundo(){
     return (<img
-        src="images/ImagemFundo.png"
+        src="/images/ImagemFundo"
         alt="Fundo"
         className="absolute inset-0 w-full h-full object-cover -z-10"
       />);
