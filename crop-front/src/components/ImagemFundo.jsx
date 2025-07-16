4454f66938bc351822f1782c13c25d4075c7b426
Matheus <matheus.filippipolinski@gmail.com>
2025-07-16 14:41:53 -0300

    fundo do cadastro
    arrumada a imagem de fundo

--- crop-front/src/components/ImagemFundo.jsx
+++ crop-front/src/components/ImagemFundo.jsx
@@ -1,6 +1,6 @@
 export default function ImagemFundo(){
     return (<img
-        src="/images/ImagemFundo"
+        src="/images/ImagemFundo.png"
         alt="Fundo"
         className="absolute inset-0 w-full h-full object-cover -z-10"
       />);
