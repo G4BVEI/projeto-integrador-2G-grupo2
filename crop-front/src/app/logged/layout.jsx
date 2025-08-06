7ef93fadedc4127021a69eee28da51d47e5ffd49
G4BVEI <gabdaveiga@gmail.com>
2025-08-06 14:28:24 -0300

    imports

--- crop-front/src/app/logged/layout.jsx
+++ crop-front/src/app/logged/layout.jsx
@@ -1,7 +1,11 @@
+import LoggedHeader from "@/components/LoggedHeader";
+import Sidebar from "@/components/Sidebar";
 
 export default function LoggedLayout({ children }) {
   return (
     <>
+      <LoggedHeader></LoggedHeader>
+      <Sidebar></Sidebar>
       {children}
     </>
   );
