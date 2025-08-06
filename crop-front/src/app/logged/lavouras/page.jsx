2fe98312f12004265fa8f5d1ea84f8be51a957b5
LuanMartini <luancarlosmartini@gmail.com>
2025-08-06 15:16:37 -0300

    certoooooooooooooooooooooooooooo

--- crop-front/src/app/logged/lavouras/page.jsx
+++ crop-front/src/app/logged/lavouras/page.jsx
@@ -4,30 +4,29 @@ import talhoes from "@/data/talhoes.json";
 function ListaTalhoes() {
 
     return (
-    <div>
-      {talhoes.length > 0 && (
-        <table>
-          <thead>
-            <tr>
-              <th>Talhão</th>
-              <th>Tipo de Cultura</th>
-              <th>Umidade do Solo</th>
-              <th>Última Atividade</th>
-            </tr>
-          </thead>
-          <tbody>
-            {talhoes.map((talhao, index) => (
-              <tr key={index}>
-                <td>{talhao.nome_do_talhao}</td>
-                <td>{talhao.tipo_de_cultura}</td>
-                <td>{talhao.umidade_do_solo}</td>
-                <td>{talhao.ultima_atividade}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      )}
-    </div>
+  <div className="flex justify-center overflow-x-auto">
+  <table className="table-auto border-collapse border border-gray-300">
+    <thead className="bg-gray-100">
+      <tr>
+        <th className="border px-4 py-2">Talhão</th>
+        <th className="border px-4 py-2">Tipo de Cultura</th>
+        <th className="border px-4 py-2">Umidade do Solo</th>
+        <th className="border px-4 py-2">Última Atividade</th>
+      </tr>
+    </thead>
+    <tbody>
+      {talhoes.map((talhao, index) => (
+        <tr key={index}>
+          <td className="border px-4 py-2">{talhao.nome_do_talhao}</td>
+          <td className="border px-4 py-2">{talhao.tipo_de_cultura}</td>
+          <td className="border px-4 py-2">{talhao.umidade_do_solo}</td>
+          <td className="border px-4 py-2">{talhao.ultima_atividade}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+</div>
+
   );
 
 }
