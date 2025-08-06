213031f4036701641a6ac0868ad450f187dbe986
G4BVEI <gabdaveiga@gmail.com>
2025-08-06 13:56:18 -0300

    ?

--- README.md
+++
@@ -1,2 +0,0 @@
-# Crop-Sense
-a https://projeto-integrador-2-g-grupo2.vercel.app
---
+++ crop-front/src/app/logged/dashboard/page.jsx
@@ -0,0 +1,11 @@
+"use client";
+import Dashboard from '@/components/Dashboard';
+import { Home, Leaf, Wrench, Cloud, Info, Menu } from 'lucide-react';
+import 'leaflet/dist/leaflet.css';
+
+
+export default function DashboardPage() {
+    return(
+        <p>falta fazer</p>
+        <Dashboard></Dashboard>
+    )}
