9570754bc691e45bf29513a19a65c2cfd22678a6
G4BVEI <gabdaveiga@gmail.com>
2025-08-20 16:12:54 -0300

    commit

--- crop-front/src/app/logged/clima/page.jsx
+++ crop-front/src/app/logged/clima/page.jsx
@@ -1,7 +1,13 @@
-import talhoes from "@/data/talhoes.json";
 
-
-function Clima() {}
+function Clima() {
+    return (
+        <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-2xl font-bold mb-4">Página de Clima</h1>
+            <p>Informações climáticas serão exibidas aqui.</p>
+            {/* Aqui você pode adicionar componentes ou lógica para exibir dados climáticos */}
+        </div>
+    )
+}
 
 
 export default Clima
