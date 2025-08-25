8c0c5fe8435225c1c14c7763da0c4c35f717a027
G4BVEI <gabdaveiga@gmail.com>
2025-08-25 17:40:22 -0300

    bah

--- crop-front/src/app/auth/cadastro/page.jsx
+++ crop-front/src/app/auth/cadastro/page.jsx
@@ -1,19 +1,12 @@
-export const dynamic = "force-dynamic";
 import ImagemFundo from "@/components/auth/ImagemFundo";
 import RegisterForm from "@/components/auth/RegisterForm";
 
-export default function Register(){
-  const handleGoogleLogin = async () => {
-    const { error } = await supabase.auth.signInWithOAuth({
-      provider: "google",
-      options: {
-        redirectTo: `${window.location.origin}/auth/callback`,
-      },
-    });
+const Register = () => {
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       <ImagemFundo />
-      <RegisterForm onGoogleLogin={handleGoogleLogin}/>
+      <RegisterForm />
     </div>
   );
-}}
+};
+export default Register;
