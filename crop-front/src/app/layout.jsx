8a72f5ac9840712ea4b2bff88bedad67f9b610d5
G4BVEI <gabdaveiga@gmail.com>
2025-08-26 10:10:14 -0300

    aaah

--- crop-front/src/app/layout.jsx
+++ crop-front/src/app/layout.jsx
@@ -1,6 +1,5 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Footer from "../components/layout/Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,7 +21,6 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden`}>
         {children}
-        <Footer />
       </body>
     </html>
   );
